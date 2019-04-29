c61fa2f16db1fcea817a685177e08f47f2a06331
sjy <929497487@qq.com>
2019-04-29 09:39:15 +0800

    实现跳转

---
+++ lib/library/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import url, include
+from . import views
+app_name = 'library'
+
+urlpatterns = [
+    url(r'^index/$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logins/$', views.logins, name='logins'),
+    url(r'^logintool/$', views.logintool, name='logintool'),
+    url(r'^book/$', views.books, name='books'),
+    url(r'^book/(\d+)/$', views.book, name='book'),
+    url(r'^user/$', views.users, name='users'),
+    url(r'^modify/(\d+)/$', views.modify, name='modify'),
+    url(r'^judge/$', views.judge, name='judge'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^register_tool/$', views.register_tool, name='register_tool'),
+    url(r'^query/(\d+)/$', views.query, name='query'),
+    url(r'^info/(\d+)/$', views.info, name='info'),
+    url(r'^history/(\d+)/$', views.history, name='history'),
+    url(r'^reader/(\d+)/$', views.reader, name='reader'),
+    url(r'^mod/(\d+)/$', views.mod, name='mod'),
+    url(r'^book_info/(\d+)/(\d+)/$', views.book_info, name='book_info'),
+]
